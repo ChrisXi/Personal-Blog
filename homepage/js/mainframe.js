741d79a16e172e1dd0deff76c3fe3208136ba094
Xi Yang <clcblueljw@gmail.com>
2016-05-20 01:31:23 -0400

    add shortcut for open/close sidebar (finish uncommited files)

--- homepage/js/mainframe.js
+++ homepage/js/mainframe.js
@@ -1,30 +1,68 @@
+var isClosed;
 $(document).ready(function () {
   var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+      overlay = $('.overlay');
+  isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
+  trigger.click(function () {
+    hamburger_cross();      
+  });
 
-    function hamburger_cross() {
+  function hamburger_cross() {
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        isClosed = true;
-      }
+    if (isClosed == true) {          
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {   
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
   }
   
   $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
+    $('#wrapper').toggleClass('toggled');
   });  
+});
+
+/**
+ * bind shortcut for open/close the sidebar
+ * <- / esc : close the sidebar
+ * ->       : open the sidebar
+ */
+$(document).bind('keydown', function(e) {  
+
+  var trigger = $('.hamburger');
+  var overlay = $('.overlay');
+
+  var key  = e.keyCode;
+  if((key == 27 || key == 37) && isClosed == true) {
+    overlay.hide();
+    trigger.removeClass('is-open');
+    trigger.addClass('is-closed');
+    isClosed = false;
+    $('#wrapper').toggleClass('toggled');
+  } else if(key == 39 && isClosed == false) {
+    overlay.show();
+    trigger.removeClass('is-closed');
+    trigger.addClass('is-open');
+    isClosed = true;
+    $('#wrapper').toggleClass('toggled');
+  }
+  
+  
+});
+
+/**
+ * Keep dropdown menu open when click the subitem in it.
+ */
+$('.dropdown .dropdown-menu').on({
+  "click":function(e){
+      e.stopPropagation();
+    }
 });
 
 
@@ -36,12 +74,12 @@ $(document).ready(function () {
     var dropdown = $('.dropdown');
 
     // Add slidedown animation to dropdown
-    dropdown.on('show.bs.dropdown', function(e){
+    dropdown.on('show.bs.dropdown', function(){
         $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
     });
 
     // Add slideup animation to dropdown
-    dropdown.on('hide.bs.dropdown', function(e){
+    dropdown.on('hide.bs.dropdown', function(){
         $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
     });
 })(jQuery);
