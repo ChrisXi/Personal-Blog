ab6e082a139f26189b8abf899f3dddadae4c1ec0
Xi Yang <clcblueljw@gmail.com>
2016-05-21 15:05:37 -0400

    finish devil minion (works in chrome, but not in local file

--- homepage/js/index.js
+++ homepage/js/index.js
@@ -1,6 +1,12 @@
+var release = false;
+
 $( "#mousenose" ).click(function() {
 	// alert("dd");
-	window.top.$('html, body').animate({scrollTop: 550}, 500);
+	if (release == false) {
+		window.top.$('html, body').animate({scrollTop: 550}, 500);
+	} else {
+		window.top.$('html, body').animate({scrollTop: 55}, 500);
+	}
 	// $('#dropDownSpan', window.parent.document).css('visibility', 'visible');
 	// $('html, body', window.parent.document).animate({scrollTop: 550}, 500);
 	// $('#btn', window.parent.document).css("background-color", "yellow");
