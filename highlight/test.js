6fed6d1754fe9cc8f246bf733f18c1ce84829a10
Xi Yang <clcblueljw@gmail.com>
2016-05-24 18:31:25 -0400

    add btn test

--- highlight/test.js
+++ highlight/test.js
@@ -1,14 +1,23 @@
 window.onload = function() {
-    var aCodes = document.getElementsByTagName('pre');
+    // var aCodes = document.getElementsByTagName('pre');
 	
-    for (var i=0; i < aCodes.length; i++) {
-        hljs.highlightBlock(aCodes[i]);
-    }
-    alert("asdf");
+    // for (var i=0; i < aCodes.length; i++) {
+    //     hljs.highlightBlock(aCodes[i]);
+    // }
+    // alert("asdf");
 };
 
 $(document).ready(function () {
 	alert("bbb");
 	$("#di").load("test.txt");
     $("#div1").load("test.txt");
+
+    $("button").click(function(){
+        var aCodes = document.getElementsByTagName('pre');
+	
+	    for (var i=0; i < aCodes.length; i++) {
+	        hljs.highlightBlock(aCodes[i]);
+	    }
+	    alert("asdf");
+    });
 });
