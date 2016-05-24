458af3d78cb3d01183621ee26b7b4e92eff883d0
Yang Xi <yxi5@gradx.cs.jhu.edu>
2016-05-24 18:21:39 -0400

    test code

--- highlight/test.js
+++ highlight/test.js
@@ -1,6 +1,12 @@
 window.onload = function() {
     var aCodes = document.getElementsByTagName('pre');
+$("#di").load("test.txt");
+        $("#div1").load("test.txt");
+    alert("dddd");
     for (var i=0; i < aCodes.length; i++) {
         hljs.highlightBlock(aCodes[i]);
     }
+    
+
+    alert("asdf");
 };
