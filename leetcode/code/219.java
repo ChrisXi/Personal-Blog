30fa7f8986032d21dbf06b393acb6cec8c9f5fd8
Xi Yang <clcblueljw@gmail.com>
2016-05-26 20:49:08 -0400

    add leetcode 219 (2)

---
+++ leetcode/code/219.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> m = new HashMap<Integer, Integer>();
+        
+        for(int i=0; i<nums.length; i++) {
+            if (m.containsKey(nums[i])) {
+                int j = m.get(nums[i]);
+                if(i-j <= k) {
+                    return true;
+                } else {
+                    m.put(nums[i], i);
+                }
+            } else {
+                m.put(nums[i], i);
+            }
+        } 
+        
+        return false;
+    }
+}
