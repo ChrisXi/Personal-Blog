e7cd7e5581aa21ce4262ce0c1301e4c8c46a4953
Xi Yang <clcblueljw@gmail.com>
2016-06-06 01:22:11 -0400

    add leetcode 330

---
+++ leetcode/code/330.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int minPatches(int[] nums, int n) {
+        
+        long miss = 1;
+        int i = 0;
+        int add = 0;
+        while (miss <= n) {
+            
+            if (i<nums.length && nums[i]<=miss) {
+                miss += nums[i];
+                i++;
+            } else {
+                miss *= 2;
+                add += 1;
+            }
+        }
+        
+        return add;   
+    }
+}
