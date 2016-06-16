752f5351ef7da986ba9dc9672e2599599d29b5ca
Xi Yang <clcblueljw@gmail.com>
2016-06-16 15:59:31 -0400

    add leetcode 50

---
+++ leetcode/code/50.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public double myPow(double x, int n) {
+     
+        return pow(x, (long) n);
+    }
+    
+    double pow(double x, long n) {
+        if (n == 0) return 1;
+        
+        if (n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        
+        return (n%2 == 0) ? pow(x*x, n/2):x*pow(x*x, n/2);
+    }
+}
