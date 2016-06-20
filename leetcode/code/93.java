9d2d4666023c3fcf87329606f6c70699919117f3
Xi Yang <clcblueljw@gmail.com>
2016-06-20 15:56:40 -0400

    renew leetcode 93

--- leetcode/code/93.java
+++ leetcode/code/93.java
@@ -1,38 +1,37 @@
 public class Solution {
-    
     public List<String> restoreIpAddresses(String s) {
+        
         List<String> ans = new ArrayList<String>();
         
-        getIP(ans, new ArrayList<Integer>(), s);
+        add (ans, new ArrayList<Integer>(), s);
         
         return ans;
     }
-
-    void getIP(List<String> ans, ArrayList<Integer> ips, String str) {
+    
+    void add (List<String> ans, List<Integer> ips, String s) {
         
-        if(ips.size() == 4) {
-            if(str.equals("")) {
-                ans.add(ips.get(0)+"."+ips.get(1)+"."+ips.get(2)+"."+ips.get(3));
-            } 
+        int len = Math.min(s.length(),3);
+        
+        if (ips.size() > 4) return;
+        
+        if (len == 0) {
+            if (ips.size() == 4) {
+                StringBuilder str = new StringBuilder("");
+                str.append(ips.get(0)+"."+ips.get(1)+"."+ips.get(2)+"."+ips.get(3));
+                ans.add(str.toString());
+            }
             return;
         }
         
-        int len=1;
-        while(str.length()>=len) {
-            int ip = Integer.parseInt(str.substring(0, len));
-            if(ip<=255) {
-                ips.add(ip);
-                getIP(ans, ips, str.substring(len));
+        len = (s.charAt(0)=='0' ? 1:len);
+        int n = 0;
+        for (int i=0; i<len; i++) {
+            n = n*10 + (s.charAt(i)-'0');
+            if (n <= 255) {
+                ips.add(n);
+                add(ans, ips, s.substring(i+1));
                 ips.remove(ips.size()-1);
-                
-                if(ip != 0) {
-                    len++;
-                } else {
-                    break;
-                }
-            } else {
-                break;
-            }
+            } else break;
         }
     }
 }
