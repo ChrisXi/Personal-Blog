8aee81d1b7083abcdd05f1c8cd42526bd8eb1714
Xi Yang <clcblueljw@gmail.com>
2016-06-28 15:56:22 -0400

    add leetcode 124

--- leetcode/code/124.java
+++ leetcode/code/124.java
@@ -8,32 +8,22 @@
  * }
  */
 public class Solution {
+    
+    int ans = Integer.MIN_VALUE;
+    
     public int maxPathSum(TreeNode root) {
-        
-        int[] val = search(root);
-        return Math.max(val[0], val[1]);
+            
+        maxChild(root);
+        return ans;
     }
     
-    int[] search(TreeNode root) {
+    int maxChild(TreeNode root) {
+        if (root == null) return 0;
+        int l = Math.max(0, maxChild(root.left));
+        int r = Math.max(0, maxChild(root.right));
         
-        int[] node = new int[2];
-        if (root == null)
-            return node;
-            
-        int[] left = search(root.left);
-        int[] right = search(root.right);
-        int val = root.val;
+        ans = Math.max(ans, l+root.val+r);
         
-        left[0] = root.left==null ? Integer.MIN_VALUE:left[0];
-        right[0] = root.right==null ? Integer.MIN_VALUE:right[0];
-        
-        node[0] = Math.max(left[1]+val+right[1],
-                  Math.max(left[1]+val, 
-                  Math.max(right[1]+val,
-                  Math.max(right[0],left[0])))); //with root node
-        node[1] = Math.max(val, 
-                  Math.max(val+left[1], val+right[1])); //without root node
-                  
-        return node;
+        return Math.max(l,r)+root.val;
     }
 }
