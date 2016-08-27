008fd779492bb2ec493c2a8e57adbe277163f1e6
Xi Yang <clcblueljw@gmail.com>
2016-08-27 14:51:07 -0400

    add leetcode 276

---
+++ leetcode/code/276.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int numWays(int n, int k) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return k;
+        int diff = k*(k-1), same = k;
+        
+        for (int i=3; i<=n; i++) {
+            int temp = diff;
+            diff = (diff+same)*(k-1);
+            same = temp;
+        }
+        
+        return diff+same;
+    }
+}
