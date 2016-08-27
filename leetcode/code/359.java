f41844070591b747b28523af3f47de8193e2643a
Xi Yang <clcblueljw@gmail.com>
2016-08-27 11:33:55 -0400

    add leetcode 359

---
+++ leetcode/code/359.java
@@ -0,0 +1,28 @@
+public class Logger {
+
+    /** Initialize your data structure here. */
+    HashMap<String, Integer> map;
+    public Logger() {
+        map = new HashMap<String, Integer>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        int lastTime = map.getOrDefault(message, -10);
+        
+        if (timestamp - lastTime >= 10) {
+            map.put(message, timestamp);
+            return true;
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
