1ef8d81689cdae98f15e47178de74c1f85f69ec6
Xi Yang <clcblueljw@gmail.com>
2016-08-29 10:23:32 -0400

    add leetcode 384

---
+++ leetcode/code/384.java
@@ -0,0 +1,37 @@
+public class Solution {
+
+    int[] nums;
+    Random rand;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        rand = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return this.nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] ans = this.nums.clone();
+        for (int i=0; i<ans.length; i++) {
+            int j = rand.nextInt(ans.length);
+            swap(ans, i, j);
+        }
+        return ans;
+    }
+    
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
